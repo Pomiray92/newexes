b8b18fb765f88be9d705169e8f699a4a5bf07644
Somon Gulmamadov <soma.gulov@gmail.com>
2022-08-31 13:16:53 +0200

    green dot

--- ex3/ex9.py
+++ ex3/ex9.py
@@ -6,6 +6,8 @@ secret_year = "1982"
 for user in range(3):
     user = input("Do you wont to now my secret? ")
 
+    # How are you Somon
+
     if user == "*secret:*":
         print(secret[::-1])
 
