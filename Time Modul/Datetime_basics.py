db7983a9a1ae87a63cf3957c3c6841c9d148eee2
Somon Gulmamadov <soma.gulov@gmail.com>
2022-09-15 16:19:36 +0200

    datetime_Modul

---
+++ Time Modul/Datetime_basics.py
@@ -0,0 +1,43 @@
+
+import datetime
+#from datetime import datetime
+#import calendar
+
+# # Task 1
+
+# current_datetime = datetime.datetime.now()
+# print(current_datetime.year)
+# print(current_datetime.strftime("%A"))
+
+# # Task 2
+
+# some_date = datetime.datetime(2021, 7, 14)
+
+# weekday = some_date.weekday()
+
+# print(weekday)
+
+# Task 3
+
+# user_imp = int(input("Enter  a Year: "))
+
+# check_imp = calendar.isleap(user_imp)
+# if check_imp == True:
+#     print("The Year", user_imp, "is a Leap: ")
+# else:
+#     print("The Year", user_imp, "is not Leap: ")
+
+
+# Task 4
+
+# user_imp = input("Enter a date in this format: Mmm dd jjjj hh:mmAM/PM: ")
+
+# date_as_string = datetime.strptime(user_imp,"%b %d %Y %H:%M")
+
+# print("The time: ", date_as_string)
+
+# Task 5
+
+Current_datetime = datetime.datetime.now()
+
+sub_days = datetime.(15)
