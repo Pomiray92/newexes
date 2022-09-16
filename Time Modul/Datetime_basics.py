aa77790b75a8017aed736e8ebaa02b90c9bf5607
Somon Gulmamadov <soma.gulov@gmail.com>
2022-09-16 12:53:14 +0200

    task 5

--- Time Modul/Datetime_basics.py
+++ Time Modul/Datetime_basics.py
@@ -1,7 +1,6 @@
-
 import datetime
-#from datetime import datetime
-#import calendar
+from datetime import *
+# import calendar
 
 # # Task 1
 
@@ -19,25 +18,45 @@ import datetime
 
 # Task 3
 
-# user_imp = int(input("Enter  a Year: "))
+# user_inp = int(input("Enter  a Year: "))
 
-# check_imp = calendar.isleap(user_imp)
-# if check_imp == True:
-#     print("The Year", user_imp, "is a Leap: ")
+# check_inp = calendar.isleap(user_inp)
+# if check_inp == True:
+#     print("The Year", user_inp, "is a Leap: ")
 # else:
-#     print("The Year", user_imp, "is not Leap: ")
+#     print("The Year", user_inp, "is not Leap: ")
 
 
 # Task 4
 
-# user_imp = input("Enter a date in this format: Mmm dd jjjj hh:mmAM/PM: ")
+# user_inp = input("Enter a date in this format: Mmm dd jjjj hh:mmAM/PM: ")
 
-# date_as_string = datetime.strptime(user_imp,"%b %d %Y %H:%M")
+# date_as_string = datetime.strptime(user_inp,"%b %d %Y %H:%M")
 
 # print("The time: ", date_as_string)
 
 # Task 5
 
-Current_datetime = datetime.datetime.now()
+# current_datetime = datetime.datetime.now()
 
-sub_days = datetime.(15)
+# sub_days = current_datetime - datetime.timedelta(days=15)
+
+# print(sub_days)
+
+# Task 6
+
+# current_datetime = datetime.datetime.now()
+
+# sub_days = current_datetime + datetime.timedelta(days=7)
+
+# print(sub_days)
+
+# Task 7 
+
+user_inp = input("Type the time of moving: Mmm dd jjjj: ")
+
+string_as_date = datetime.strptime(user_inp,"%b %d %Y")
+
+day_of_pay = string_as_date + datetime.timedelta(days=25)
+
+print("Hello Friedrich, your rent of 300 € is due on:", day_of_pay)
